f4c860411607364a95292e83854006f78fb2198f
BSarahB <bslajta@gmail.com>
2019-10-11 00:26:24 +0200

    adding rubocop rules

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -6,21 +6,21 @@ require 'open-uri'
 def crypto_scrapper
   page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")) 
  
-  arr_names = []
-  arr_prices = []
+  crypto_names = []
+  crypto_prices = []
 
   arr_collecting_names = page.xpath('//*[@class="text-left col-symbol"]')
   arr_collecting_names.each do |name_link|
-    arr_names << name_link.text
+    crypto_names << name_link.text
   end
 
 
   arr_collecting_prices = page.xpath('//*[@class="price"]')
   arr_collecting_prices.each do |price_link|
-    arr_prices << price_link.text
+    crypto_prices << price_link.text
 end
 
-  zipped_array = arr_names.zip(arr_prices)
+  zipped_array = crypto_names.zip(crypto_prices)
   array_of_hashes = []
   zipped_array.each do |element| 
     array_of_hashes << {element[0] => element[1]} 

fc9e44daa6cd6d95e46b7e8fdb7e9898198e1c6f
BSarahB <bslajta@gmail.com>
2019-10-11 00:22:51 +0200

    adding rubocop rules

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -1,4 +1,3 @@
-
 require 'nokogiri'
 require 'open-uri'
 
@@ -7,28 +6,28 @@ require 'open-uri'
 def crypto_scrapper
   page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")) 
  
-arr_n = []
-arr_p = []
+  arr_names = []
+  arr_prices = []
 
-arr_1 = page.xpath('//*[@class="text-left col-symbol"]')
-arr_1.each do |name_link|
-arr_n << name_link.text
+  arr_collecting_names = page.xpath('//*[@class="text-left col-symbol"]')
+  arr_collecting_names.each do |name_link|
+    arr_names << name_link.text
   end
 
 
-arr_2 = page.xpath('//*[@class="price"]')
-arr_2.each do |price_link|
-arr_p << price_link.text
+  arr_collecting_prices = page.xpath('//*[@class="price"]')
+  arr_collecting_prices.each do |price_link|
+    arr_prices << price_link.text
 end
 
-zipped_array = arr_n.zip(arr_p)
-array_of_hashes = []
-zipped_array.each do |element| 
-  array_of_hashes << {element[0] => element[1]} 
+  zipped_array = arr_names.zip(arr_prices)
+  array_of_hashes = []
+  zipped_array.each do |element| 
+    array_of_hashes << {element[0] => element[1]} 
 end
   
   puts array_of_hashes
-return array_of_hashes
+  return array_of_hashes
 end
 
 crypto_scrapper

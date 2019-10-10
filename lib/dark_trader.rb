5d8e8e7f72bdb55fe139b001cd9c97969ffbddcc
BSarahB <bslajta@gmail.com>
2019-10-10 18:13:52 +0200

    adding the array of hashes

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -1,7 +1,6 @@
-require 'rubygems'
+
 require 'nokogiri'
 require 'open-uri'
-
 
 
 
@@ -14,22 +13,31 @@ arr_p = []
 arr_1 = page.xpath('//*[@class="text-left col-symbol"]')
 arr_1.each do |name_link|
 arr_n << name_link.text
-
   end
+
 
 arr_2 = page.xpath('//*[@class="price"]')
 arr_2.each do |price_link|
 arr_p << price_link.text
 end
 
-
-my_hash = {}
-
-arr_n.zip(arr_p) {|key,value| my_hash[key] = value}
-p my_hash
-
- end   
+zipped_array = arr_n.zip(arr_p)
+array_of_hashes = []
+zipped_array.each do |element| 
+  array_of_hashes << {element[0] => element[1]} 
+end
   
+  puts array_of_hashes
 
+end
 
 crypto_scrapper
+
+
+
+
+
+
+
+
+

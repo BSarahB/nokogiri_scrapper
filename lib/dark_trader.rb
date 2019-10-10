e5398b1d26b66edc1bc3d09900342c9c41677ad1
BSarahB <bslajta@gmail.com>
2019-10-10 16:33:51 +0200

    simplify

--- lib/dark_trader.rb
+++ lib/dark_trader.rb
@@ -1,44 +1,35 @@
-
+require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
 
 
 
+def crypto_scrapper
+  page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/")) 
+ 
+arr_n = []
+arr_p = []
 
-def get_original_data_crypto
-  page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
-  arr_crypto_name = []
-  arr_crypto_name_original = page.xpath('//*[@class="text-left col-symbol"]')
+arr_1 = page.xpath('//*[@class="text-left col-symbol"]')
+arr_1.each do |name_link|
+arr_n << name_link.text
+
+  end
+
+arr_2 = page.xpath('//*[@class="price"]')
+arr_2.each do |price_link|
+arr_p << price_link.text
+end
+
+
+my_hash = {}
+
+arr_n.zip(arr_p) {|key,value| my_hash[key] = value}
+p my_hash
+
+ end   
   
-  arr_crypto_name_original.each do |crypto_link|
-	
-      arr_crypto_name << crypto_link.text
-    end
-
-return arr_crypto_name
-end
 
 
-def get_original_data_price
-  page = Nokogiri::HTML(open("https://coinmarketcap.com/all/views/all/"))
-  arr_prices = []
-  arr_prices_original = page.xpath('//*[@class="price"]')
-
-arr_prices_original.each do |price_link|
-	arr_prices << price_link.text
-end
-return arr_prices
-
-end
-
-def crypto_scrapper (arr_crypto_name, arr_prices)
-
-  my_hash = {}
-  arr_crypto_name.zip(arr_prices){|key,value| my_hash[key] = value}
-  puts my_hash
-end
-
-arr_crypto_name = get_original_data_crypto
-arr_prices = get_original_data_price
-crypto_scrapper(arr_crypto_name,arr_prices)
+crypto_scrapper

744f9ec1e5f4400468275a3954e8e2cf566f3a5a
BSarahB <bslajta@gmail.com>
2019-10-11 02:47:23 +0200

    adding cher_depute.rb

--- lib/cher_depute.rb
+++ lib/cher_depute.rb
@@ -3,34 +3,36 @@
 require 'nokogiri'
 require 'open-uri'
 
-def get_congres_email(congress_URL)
+def get_congres_email
   
-  array_list_urls = congress_URL
+  array_list_urls = get_congres_urls
   deputes_emails = []
   n = 0
-  576.times do # mettre un valeur 10 par exemple pour faire le test
+  576.times do #576 est array_list.urls.length
     page = Nokogiri::HTML(open(array_list_urls[n]))# ouverture d'une nouvelle page pour chaque élément du tableau
     deputes_emails << page.xpath("//*[@id='haut-contenu-page']/article/div[3]/div/dl/dd[4]/ul/li[2]/a/text()")
     n = n+1
   end
   p deputes_emails
   return deputes_emails
-
-  #array_urls = page #
+#ajoutons le hash
  #{ array_urls => list_deputes } #
 end
 
 def get_congress_names
   page = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr/deputes/liste/alphabetique"))
   list_deputes = page.xpath("//ul[@class='col3']//li//a")
+   array_list_urls = get_congres_urls
+
   array_list_deputes = []
-    #p list_deputes
+   
   list_deputes.each do |depute|
-	array_list_deputes << depute.text
+p array_list_deputes
+  
 
 end
   return array_list_deputes
-p array_list_deputes
+
 end
   
 def get_congres_urls
@@ -41,16 +43,24 @@ def get_congres_urls
 
   list_urls.each do |depute_url|
       array_list_urls << "http://www2.assemblee-nationale.fr" + depute_url['href'] #je remplis l array des liens href url de chaque depute
+ #p array_list_urls
 
 	end
-
-	#get_congress_emails(url) #on envoie sur la methode du haut
  return array_list_urls
- p array_list_urls
 end
- #array_final << get_congres_email(depute.)
+def hash(array_list_urls,array_list_deputes)
 
-congress_URL = get_congres_urls
+  zipped_array = array_list_urls.zip(array_list_deputes)
+  array_of_hashes = []
+  zipped_array.each do |element| 
+    array_of_hashes << {element[0] => element[1]} 
+end
+p array_of_hashes
+	end
+array_list_urls = get_congres_urls
+array_list_deputes = get_congress_names
+hash(array_list_urls,array_list_deputes)
 
-get_congres_email(congress_URL)
-  
+
+
+
